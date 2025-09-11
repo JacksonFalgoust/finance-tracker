640738373585d57e5a24f01c3303f33fcc7db35c
Jbub320 <38819417+Jbub320@users.noreply.github.com>
2025-09-11 02:33:12 -0400

    Create spring boot backend and connect it to tracker MySQL database.

---
+++ finance-tracker-backend/src/main/java/com/Jackson_Falgoust/finance_tracker/FinanceTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Jackson_Falgoust.finance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ finance-tracker-backend/src/test/java/com/Jackson_Falgoust/finance_tracker/FinanceTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jackson_Falgoust.finance_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
