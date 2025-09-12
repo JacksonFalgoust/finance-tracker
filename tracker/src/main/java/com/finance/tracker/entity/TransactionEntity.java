ebfe2d485dfb431c3679288c688d2e9a081e1f2f
Jbub320 <38819417+Jbub320@users.noreply.github.com>
2025-09-12 02:14:12 -0400

    Completes TransactionEntity.java

--- tracker/src/main/java/com/finance/tracker/entity/TransactionEntity.java
+++ tracker/src/main/java/com/finance/tracker/entity/TransactionEntity.java
@@ -1,5 +1,48 @@
-package entity;
+package com.finance.tracker.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import com.finance.tracker.entity.CategoryEntity;
+import com.finance.tracker.entity.AccountEntity;
+/*
+ * Entity class representing a financial transaction.
+ */
+
+@Getter
+@Setter
+@Entity
 public class TransactionEntity {
+
+    @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increment id
+    private Long id;
+
+    @ManyToOne // Many transactions can belong to one category
+    @JoinColumn(name = "category_id") // Foreign key column
+    private CategoryEntity category; // Category of the transaction linked to CategoryEntity
+
+    @ManyToOne // Many transactions can belong to one account
+    @JoinColumn(name = "account_id")  // Foreign key column
+    private AccountEntity account; // Account of the transaction linked to AccountEntity
+
+    private LocalDate date;
+    private Double amount;
+
+    private TransactionEntity() {} // Default constructor for JPA
+    private TransactionEntity(CategoryEntity category, AccountEntity account, LocalDate date, Double amount) {
+        this.category = category;
+        this.account = account;
+        this.date = date;
+        this.amount = amount;
+    }
 
 }
