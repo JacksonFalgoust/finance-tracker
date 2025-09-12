829f9994392dc8e9d48fdbf9877d9eea5a7d215b
Jbub320 <38819417+Jbub320@users.noreply.github.com>
2025-09-12 02:39:18 -0400

    Completes CategoryEntity class

--- tracker/src/main/java/com/finance/tracker/entity/CategoryEntity.java
+++ tracker/src/main/java/com/finance/tracker/entity/CategoryEntity.java
@@ -1,5 +1,32 @@
-package entity;
+package com.finance.tracker.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<TransactionEntity> transactions;
+
+    public CategoryEntity() {} // Default constructor for JPA
+    public CategoryEntity(String name) {
+        this.name = name;
+    }
 
 }
