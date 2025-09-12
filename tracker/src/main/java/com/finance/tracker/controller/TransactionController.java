497f0e11f5fc5421dc60d792f8a7f09e5e42653d
Jbub320 <38819417+Jbub320@users.noreply.github.com>
2025-09-12 16:24:30 -0400

    Started working on TransactionController

--- tracker/src/main/java/com/finance/tracker/controller/TransactionController.java
+++ tracker/src/main/java/com/finance/tracker/controller/TransactionController.java
@@ -1,5 +1,35 @@
 package com.finance.tracker.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.finance.tracker.entity.TransactionEntity;
+import com.finance.tracker.repository.AccountRepository;
+import com.finance.tracker.repository.CategoryRepository;
+import com.finance.tracker.repository.TransactionRepository;
+
+
+@RestController
+@RequestMapping("/transactions")
 public class TransactionController {
+
+    private final TransactionRepository transRepo;
+    private final CategoryRepository catRepo;
+    private final AccountRepository accRepo;
+
+    public TransactionController(TransactionRepository transRepo, CategoryRepository catRepo, AccountRepository accRepo) {
+        this.transRepo = transRepo;
+        this.catRepo = catRepo;
+        this.accRepo = accRepo;
+    }
+
+    @GetMapping
+    public List<TransactionEntity> getTransactions() {
+        return transRepo.findAll();
+    }
+    
 
 }
