4d8bff3b1fcd5d383afe94a7a126bfac47a43ec1
Jbub320 <38819417+Jbub320@users.noreply.github.com>
2025-09-14 00:08:47 -0400

    Creates basic AccountController

--- tracker/src/main/java/com/finance/tracker/controller/AccountController.java
+++ tracker/src/main/java/com/finance/tracker/controller/AccountController.java
@@ -1,5 +1,33 @@
 package com.finance.tracker.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.finance.tracker.entity.AccountEntity;
+import com.finance.tracker.repository.AccountRepository;
+import com.finance.tracker.repository.CategoryRepository;
+import com.finance.tracker.repository.TransactionRepository;
+
+@RestController
+@RequestMapping("/accounts")
 public class AccountController {
+
+    private final TransactionRepository transRepo;
+    private final CategoryRepository catRepo;
+    private final AccountRepository accRepo;
+
+    public AccountController(TransactionRepository transRepo, CategoryRepository catRepo, AccountRepository accRepo) {
+        this.transRepo = transRepo;
+        this.catRepo = catRepo;
+        this.accRepo = accRepo;
+    }
+
+    @GetMapping
+    public List<AccountEntity> getAccounts() {
+        return accRepo.findAll();
+    }
 
 }
