6bdd0499322359bc4d7dbf91862b39eaa39e43e0
Jbub320 <38819417+Jbub320@users.noreply.github.com>
2025-09-14 00:56:58 -0400

    adds showTransactionByAccount method to AccountController to fetch transactions by account ID and updates TransactionRepository with a method signature.

--- tracker/src/main/java/com/finance/tracker/controller/AccountController.java
+++ tracker/src/main/java/com/finance/tracker/controller/AccountController.java
@@ -3,12 +3,14 @@ package com.finance.tracker.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finance.tracker.entity.AccountEntity;
+import com.finance.tracker.entity.TransactionEntity;
 import com.finance.tracker.repository.AccountRepository;
 import com.finance.tracker.repository.CategoryRepository;
 import com.finance.tracker.repository.TransactionRepository;
@@ -35,6 +37,11 @@ public class AccountController {
     @PostMapping
     public AccountEntity createAccount(@RequestBody AccountEntity account) {
         return accRepo.save(account);
+    }
+
+    @GetMapping("/{accountId}")
+    public List<TransactionEntity> showTransactionByAccount(@PathVariable Long accountId) {
+        return transRepo.findByAccountId(accountId);
     }
 
 }
