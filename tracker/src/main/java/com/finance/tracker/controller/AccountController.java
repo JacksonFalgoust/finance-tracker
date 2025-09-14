c38e476dd43e537c01c13ec28cbd8fb0f31fd4e1
Jbub320 <38819417+Jbub320@users.noreply.github.com>
2025-09-14 00:27:36 -0400

    Updated basic AccountController

--- tracker/src/main/java/com/finance/tracker/controller/AccountController.java
+++ tracker/src/main/java/com/finance/tracker/controller/AccountController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +33,7 @@ public class AccountController {
     }
 
     @PostMapping
-    public AccountEntity createAccount(AccountEntity account) {
+    public AccountEntity createAccount(@RequestBody AccountEntity account) {
         return accRepo.save(account);
     }
 
