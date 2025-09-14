b69f3d663ce0d3657fa10fc2033f028595b01dc6
Jbub320 <38819417+Jbub320@users.noreply.github.com>
2025-09-14 00:10:19 -0400

    Creates basic CategoryController

--- tracker/src/main/java/com/finance/tracker/controller/CategoryController.java
+++ tracker/src/main/java/com/finance/tracker/controller/CategoryController.java
@@ -1,5 +1,40 @@
 package com.finance.tracker.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.finance.tracker.entity.CategoryEntity;
+import com.finance.tracker.repository.AccountRepository;
+import com.finance.tracker.repository.CategoryRepository;
+import com.finance.tracker.repository.TransactionRepository;
+
+@RestController
+@RequestMapping("/categories")
 public class CategoryController {
 
+
+    private final TransactionRepository transRepo;
+    private final CategoryRepository catRepo;
+    private final AccountRepository accRepo;
+
+    public CategoryController(TransactionRepository transRepo, CategoryRepository catRepo, AccountRepository accRepo) {
+        this.transRepo = transRepo;
+        this.catRepo = catRepo;
+        this.accRepo = accRepo;
+    }
+
+    @GetMapping
+    public List<CategoryEntity> getCategories() {
+        return catRepo.findAll();
+    }
+
+    @PostMapping
+    public CategoryEntity createCategory(@RequestBody CategoryEntity category) {
+        return catRepo.save(category);
+    }
 }
