32eda9a422469446e84eb2d8bdc78160108603a6
Jbub320 <38819417+Jbub320@users.noreply.github.com>
2025-09-18 00:38:26 -0400

    Creates premade categories in data.sql and ensures it runs after schema creation in application.properties

--- tracker/src/main/resources/data.sql
+++ tracker/src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO category (id, name, type) VALUES
-(1, 'Groceries', 'EXPENSE'),
-(2, 'Bills', 'EXPENSE'),
-(3, 'Dining', 'EXPENSE'),
-(4, 'Entertainment', 'EXPENSE'),
-(5, 'Salary', 'INCOME'),
-(6, 'Shopping', 'EXPENSE'),
-(7, 'Travel', 'EXPENSE');
+INSERT INTO CATEGORY_ENTITY (name, type) VALUES
+('Groceries', 'EXPENSE'),
+('Bills', 'EXPENSE'),
+('Dining', 'EXPENSE'),
+('Entertainment', 'EXPENSE'),
+('Salary', 'INCOME'),
+('Shopping', 'EXPENSE'),
+('Travel', 'EXPENSE');
