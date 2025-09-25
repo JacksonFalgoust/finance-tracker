2c6b0fb9189de28da85408c0f2f09d874705d3c0
Jbub320 <jbub320@gmail.com>
2025-09-25 14:37:33 -0400

    idk

--- tracker/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO CATEGORY_ENTITY (name, type, spent_this_month) VALUES
-('Other', 'EXPENSE', 0.0),
-('Groceries', 'EXPENSE', 0.0),
-('Bills', 'EXPENSE', 0.0),
-('Dining', 'EXPENSE', 0.0),
-('Entertainment', 'EXPENSE', 0.0),
-('Salary', 'INCOME', 0.0),
-('Shopping', 'EXPENSE', 0.0),
-('Travel', 'EXPENSE', 0.0);
